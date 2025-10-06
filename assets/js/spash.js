a9284bd4e33c1661768bb3160fa804f8f2ff1683
yesigarnez <mariamatias.menutech@gmail.com>
2025-10-06 12:53:00 -0700

    Delete assets directory

--- assets/js/spash.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    document.getElementById('splash').style.display = 'none';
-    document.getElementById('contenido').style.display = 'block';
-  }, 3500); // 3 segundos
-});
